320d1bf24e4dae3c6643c1aa1caf8819a0b70cb7
rumyantsev168 <Rmisha08len@yandex.ru>
2026-06-13 14:37:34 +0300

    Merge remote-tracking branch 'origin/main' into main

--- assets/js/textAnimation.js
+++ assets/js/textAnimation.js
@@ -1,7 +1,7 @@
 const el = document.querySelector(".animated");
 const orig = el.innerHTML;
 let frames = [];
-for (let i = 0; i <= orig.length; i++) {
+for (let i = 0; i < orig.length; i++) {
     frames.push(orig.substring(0, i) + "<span style='color: #ff4d4d'>" + orig[i] + "</span>" + orig.substring(i+1));
 };
 let c = 0, l = frames.length-1;
